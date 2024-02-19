08e44001ffc0dfc82fbd569d998cc094db85ac41
thmasibtagiyev0@gmail.com <tahmasibmt@code.edu.az>
2024-02-19 17:06:01 +0400

    A2

---
+++ tasks-2/tasks-2/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+#region task-1
+//int[] number = { 12, 10, 2, 18, 30 };
+//int sum = 0;
+//for (int i = 0; i < number.Length; i++)
+//{
+//    sum += number[i];
+//}
+//Console.WriteLine(sum);
+#endregion
+#region task-3
+//int[] num = {12, 34, 8, 5, 29, 3, 15,5};
+//int ilk = num[0];
+//int son = num[num.Length-1];
+//int sum = 0;
+//for (int i = 0; i < num.Length; i++)
+//{
+//    sum = ilk + son;
+//}
+//Console.WriteLine(sum);
+#endregion
